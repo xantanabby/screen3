0919efa1b893242799e236e9ee2c52d377056450
xantanabby <hungnp22@uef.edu.vn>
2024-11-29 15:48:36 +0700

    screen3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(SignUpApp());
@@ -182,81 +183,267 @@ class _SignUpScreen1State extends State<SignUpScreen1> {
 }
 
 // Screen 2: Profile Details
-class SignUpScreen2 extends StatelessWidget {
+
+class SignUpScreen2 extends StatefulWidget {
+  @override
+  _SignUpScreen2State createState() => _SignUpScreen2State();
+}
+
+class _SignUpScreen2State extends State<SignUpScreen2> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _controller = TextEditingController();
+  String? selectedRole; // Biến lưu trạng thái vai trò được chọn
+  TextEditingController _phoneController =
+      TextEditingController(); // Controller cho phone number
+  FocusNode _phoneFocusNode =
+      FocusNode(); // FocusNode để theo dõi trạng thái focus của ô số điện thoại
+
+  @override
+  void initState() {
+    super.initState();
+    // Lắng nghe sự thay đổi của focus trên ô phone number
+    _phoneFocusNode.addListener(() {
+      setState(() {}); // Cập nhật giao diện mỗi khi focus thay đổi
+    });
+  }
+
+  @override
+  void dispose() {
+    _phoneFocusNode.dispose(); // Đảm bảo giải phóng tài nguyên
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Sign Up"),
-          centerTitle: true,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        body: GestureDetector(
-          onTap: () {
-            FocusScope.of(context).unfocus(); // Bỏ focus, ẩn bàn phím
+      resizeToAvoidBottomInset: true, // Điều chỉnh khi bàn phím xuất hiện
+      appBar: AppBar(
+        titleSpacing: 0,
+        title: const Text("Sign Up"),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
           },
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context)
+              .unfocus(); // Đóng bàn phím khi nhấn vào vùng trống
+        },
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey, // Gắn khóa Form
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(
+                const Text(
                   "Create an account",
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 10),
-                Text(
+                const SizedBox(height: 5),
+                const Text(
                   "Please complete your profile",
-                  style: TextStyle(fontSize: 16),
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
                   textAlign: TextAlign.center,
                 ),
-                SizedBox(height: 20),
-                TextField(
-                  decoration: InputDecoration(labelText: "Full name"),
+                const SizedBox(height: 5),
+                const Text(
+                  "Don`t worry. Your data will remain private and only you can see it.",
+                  style: TextStyle(fontSize: 10),
+                  textAlign: TextAlign.center,
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Date of Birth"),
+                const SizedBox(height: 20),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Full name"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Full name cannot be empty";
+                    }
+                    return null; // Trả về null nếu hợp lệ
+                  },
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Phone Number"),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(labelText: "Date of Birth"),
+                  onChanged: (value) {
+                    if (value.length == 2 || value.length == 5) {
+                      if (value.endsWith('/')) {
+                        _controller.text = "$value/";
+                        _controller.selection = TextSelection.collapsed(
+                            offset: _controller.text.length);
+                      }
+                    }
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Date of Birth cannot be empty";
+                    }
+                    if (!RegExp(r'^\d{2}/\d{2}/\d{4}$').hasMatch(value)) {
+                      return "Enter a valid date in DD/MM/YYYY format";
+                    }
+                    return null;
+                  },
                 ),
-                TextField(
-                  decoration: InputDecoration(labelText: "Username"),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller:
+                      _phoneController, // Sử dụng controller cho phone number
+                  focusNode: _phoneFocusNode, // Gắn FocusNode vào TextField
+                  keyboardType: TextInputType.phone,
+                  inputFormatters: [
+                    FilteringTextInputFormatter
+                        .digitsOnly, // Chỉ cho phép nhập chữ số
+                  ],
+                  decoration: InputDecoration(),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Phone Number cannot be empty";
+                    }
+                    if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+                      return "Phone Number must be numeric";
+                    }
+                    if (value.length > 12) {
+                      // Kiểm tra số điện thoại phải có 12 ký tự (bao gồm +84)
+                      return "Phone Number must be 12 digits (+ 8 digits)";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    // Nếu người dùng bắt đầu với '0', thay thế nó bằng '+84'
+                    if (value.startsWith('0') && value.length > 1) {
+                      _phoneController.text = '+84' + value.substring(1);
+                      _phoneController.selection = TextSelection.collapsed(
+                          offset: _phoneController.text.length);
+                    } else if (value.isNotEmpty && !value.startsWith('0')) {
+                      // Nếu không bắt đầu bằng '0', không thay đổi gì
+                      _phoneController.text = value;
+                      _phoneController.selection = TextSelection.collapsed(
+                          offset: _phoneController.text.length);
+                    }
+                  },
+                  onTap: () {
+                    setState(() {
+                      // Khi người dùng tap vào ô nhập, ẩn phần +84
+                    });
+                  },
+                  onEditingComplete: () {
+                    // Sau khi người dùng hoàn tất nhập, nếu nhập số bắt đầu bằng '0', thay thế bằng +84
+                    if (_phoneController.text.startsWith('0')) {
+                      _phoneController.text =
+                          '+84' + _phoneController.text.substring(1);
+                      _phoneController.selection = TextSelection.collapsed(
+                          offset: _phoneController.text.length);
+                    }
+                  },
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "Username"),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Username cannot be empty";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 30),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     ElevatedButton(
-                      onPressed: () {},
-                      child: Text("Student"),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: selectedRole == "Student"
+                            ? Colors.blue
+                            : Colors.grey.shade300,
+                        foregroundColor: selectedRole == "Student"
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          selectedRole = "Student"; // Cập nhật lựa chọn
+                        });
+                      },
+                      child: Column(
+                        mainAxisSize: MainAxisSize
+                            .min, //Đảm bảo Column không chiếm quá nhiều không gian
+                        children: [
+                          Icon(
+                            Icons.person, //Icon cho "Student"
+                            color: selectedRole == "Student"
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                          SizedBox(height: 8),
+                          const Text("Student"),
+                        ],
+                      ),
                     ),
                     ElevatedButton(
-                      onPressed: () {},
-                      child: Text("Teacher"),
-                    ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: selectedRole == "Teacher"
+                            ? Colors.blue
+                            : Colors.grey.shade300,
+                        foregroundColor: selectedRole == "Teacher"
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          selectedRole = "Teacher"; // Cập nhật lựa chọn
+                        });
+                      },
+                      child: Column(
+                        mainAxisSize: MainAxisSize
+                            .min, //Đảm bảo Column không chiếm quá nhiều không gian
+                        children: [
+                          Icon(
+                            Icons.school,
+                            color: selectedRole == "Teacher"
+                                ? Colors.white
+                                : Colors.black,
+                          ),
+                          const Text("Teacher"),
+                        ],
+                      ),
+                    )
                   ],
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SignUpScreen3()),
-                    );
+                    // Kiểm tra tính hợp lệ của Form
+                    if (_formKey.currentState!.validate()) {
+                      // Nếu tất cả hợp lệ
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignUpScreen3(),
+                        ),
+                      );
+                    } else {
+                      // Nếu có trường không hợp lệ
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text("Please fill out all fields")),
+                      );
+                    }
                   },
-                  child: Text("Continue"),
+                  child: const Text("Continue"),
                 ),
               ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
@@ -405,6 +592,10 @@ class _SignUpScreen3State extends State<SignUpScreen3> {
                 controller: _newPasswordController,
                 focusNode: focusNodeNewPassword, // Gắn focus node
                 obscureText: !_isNewPassWordVisible,
+                inputFormatters: [
+                  FilteringTextInputFormatter.deny(
+                      RegExp(r'\s')), // Loại bỏ ký tự khoảng trắng
+                ],
                 style: TextStyle(
                   fontSize: 24,
                   letterSpacing: 2.0,
@@ -436,6 +627,10 @@ class _SignUpScreen3State extends State<SignUpScreen3> {
                 controller: _confirmPasswordController,
                 focusNode: focusNodeConfirmPassword, // Gắn focus node
                 obscureText: !_isConfirmPasswordVisible,
+                inputFormatters: [
+                  FilteringTextInputFormatter.deny(
+                      RegExp(r'\s')), // Loại bỏ ký tự khoảng trắng
+                ],
                 style: TextStyle(
                   fontSize: 24,
                   letterSpacing: 2.0,
